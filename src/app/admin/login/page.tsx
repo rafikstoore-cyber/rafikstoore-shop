af4c1c2b8a2933c8f9c3409d632fc118be64af04
rafikstoore-cyber <rafikstoore@gmail.com>
2026-08-26 01:05:17 +0100

    Update page.tsx

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -49,7 +49,8 @@ export default function AdminLoginPage() {
       return;
     }
 
-    router.push("/admin/dashboard");
+    - router.push("/admin/dashboard");
++ router.push("/admin");
   }
 
   return (
